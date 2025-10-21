770f66f37f4c72da2298116e9f724c9fa746def1
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-10-22 01:06:18 +0300

    created authRoutes.js
    created routes files and described the register route

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const registerUser = require('../controllers/authController');
+
+const router = express.Router();
+
+// @route   POST /api/auth/register
+router.post('/register', registerUser);
+
+
+
+
+
+module.exports = router;
