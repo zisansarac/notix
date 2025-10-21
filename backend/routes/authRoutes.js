cbae5367e85cc5719ed9287672cc78c70c2d1e8e
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-10-22 01:35:16 +0300

    updated authRoutes.js
    added login route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 const router = express.Router();
 
 // @route   POST /api/auth/register
 router.post('/register', registerUser);
 
-
-
+// @route   POST /api/auth/login
+router.post('/login', loginUser);
 
 
 module.exports = router;
