bbbb562994131973ff982f218fbc6ab14c417e9c
Zişan Saraç <87035396+zisansarac@users.noreply.github.com>
2025-10-22 14:38:51 +0300

    updated server.js
    defined the note routes on Express server

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,30 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const { connectDB } = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const noteRoutes = require('./routes/noteRoutes'); 
 
-// 1. Ortam değişkenlerini yükledim
+
 dotenv.config();
 
-// 2. Veritabanı bağlantısını kurdum
-connectDB(); // Uygulama başlatılırken bağlantıyı dener
+connectDB(); 
 
-// 3. Express uygulamasını başlattım
 const app = express();
 
-// 4. JSON verilerini işlemek için ara katman yazılımı(middleware) ekledim
-app.use(cors()); // CORS middleware'ini ekledim
+
+app.use(cors()); 
 app.use(express.json());
 
-// 5. Rota tanımı: /api/auth altında authRoutes'ı kullan
-app.use('/api/auth', authRoutes);
 
-// 6. Temel bir başlangıç rotası (Root Route) tanımladım
+app.use('/api/auth', authRoutes);
+app.use('/api/notes', noteRoutes);
+
+
 app.get('/', (req, res) => {
     res.send('Notix API çalışıyor!');
 }
 );
 
-// 7. Sunucuyu belirtilen portta dinlemeye başladım
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
